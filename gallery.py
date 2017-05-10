ce2451ed4551f1495d7070b28a16569a9068d70a
gallery <gallery@na.na>
2017-05-10 04:10:24 -0500

    doodling about with webpy templates

--- gallery.py
+++ gallery.py
@@ -1,13 +1,20 @@
 import web
 
 urls = (
-	'/', 'index'
+	'/', 'index',
+	'/register', 'register'
 )
 
-app = web.application(urls, globals())
 
 class index:
 	def GET(self):
-		return "<html><head><title>Gallery Install Successful</title></head><body><p>Heya!</p><p>If you're seeing this, setup.sh worked and your box is all set to go.</p><p>Of course, there's still plenty to do.  Check <a href=https://github.com/almafeta/gallery>the Github repository</a>, particularly <a href=https://github.com/almafeta/gallery/issues>issues</a>, for things to do from here.</p><p>- Shanya</p></body></html>"
+		render = web.template.render('templates', base='layout')
+		return render.index()
 
+class register:
+	def GET(self):
+		render = web.template.render('templates', base='layout')
+		return render.register()
+
+app = web.application(urls, globals())
 application = app.wsgifunc()
